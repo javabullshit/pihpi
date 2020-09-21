e71e370f235f2b8836461babf060595218354541
javabullshit <71615720+javabullshit@users.noreply.github.com>
2020-09-21 11:41:29 +0800

    Update archive.php

--- archive.php
+++ archive.php
@@ -4,12 +4,12 @@
 
 <div class="row">
     <div class="col-mb-12 col-8">
-        <h3 class="archive-title"><?php $this->archiveTitle(array(
+        <h2 class="archive-title"><?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
             'tag'       =>  _t('标签 %s 下的文章'),
             'author'    =>  _t('%s 发布的文章')
-        ), '', ''); ?></h3>
+        ), '', ''); ?></h2>
         <?php if ($this->have()): ?>
         <?php while($this->next()): ?>
         <div class="post-item">
